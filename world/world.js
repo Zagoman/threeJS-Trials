0a89f89da0719a9d46a2d1a59f3246ab5630bb41
Zagoman <lucaszago.ph@gmail.com>
2022-03-12 20:03:25 +0100

    set up basic scene

---
+++ world/world.js
@@ -0,0 +1,92 @@
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.118/build/three.module.js";
+
+import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.118/examples/jsm/controls/OrbitControls.js";
+
+class BasicWorld {
+  constructor() {
+    this._Initialize();
+  }
+
+  _Initialize() {
+    this._renderer = new THREE.WebGLRenderer({
+      antialias: true,
+    });
+    this._renderer.shadowMap.enabled = true;
+    this._renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+    this._renderer.setPixelRatio(window.devicePixelRatio);
+    this._renderer.setSize(window.innerWidth, window.innerHeight);
+
+    document.body.appendChild(this._renderer.domElement);
+    window.addEventListener(
+      "resize",
+      () => {
+        this._OnWindowResize();
+      },
+      false
+    );
+
+    const fov = 60;
+    const aspect = 1920 / 1080;
+    const near = 1.0;
+    const far = 1000.0;
+    this._camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    this._camera.position.set(75, 20, 0);
+
+    this._scene = new THREE.Scene();
+
+    let light = new THREE.DirectionalLight(0xffffff, 1.0);
+    light.position.set(20, 100, 10);
+    light.target.position.set(0, 0, 0);
+    light.castShadow = true;
+    light.shadow.bias = -0.001;
+    light.shadow.mapSize.width = 2048;
+    light.shadow.mapSize.height = 2048;
+    light.shadow.camera.near = 0.1;
+    light.shadow.camera.far = 500.0;
+    light.shadow.camera.near = 0.5;
+    light.shadow.camera.far = 500.0;
+    light.shadow.camera.left = 100;
+    light.shadow.camera.right = -100;
+    light.shadow.camera.top = 100;
+    light.shadow.camera.bottom = -100;
+    this._scene.add(light);
+
+    light = new THREE.AmbientLight(0x101010);
+    this._scene.add(light);
+
+    const controls = new OrbitControls(this._camera, this._renderer.domElement);
+    controls.target.set(0, 0, 0);
+    controls.update();
+
+    const loader = new THREE.CubeTextureLoader();
+    const texture = loader.load([
+      "./resources/posx.jpeg",
+      "./resources/negx.jpeg",
+      "./resources/posy.jpeg",
+      "./resources/negy.jpeg",
+      "./resources/posz.jpeg",
+      "./resources/negz.jpeg",
+    ]);
+    this._scene.background = texture;
+
+    this._RAF();
+  }
+
+  _OnWindowResize() {
+    this._camera.aspect = window.innerWidth / window.innerHeight;
+    this._camera.updateProjectionMatrix();
+    this._renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+  _RAF() {
+    requestAnimationFrame(() => {
+      this._renderer.render(this._scene, this._camera);
+      this._RAF();
+    });
+  }
+}
+
+let _APP = null;
+
+window.addEventListener("DOMContentLoaded", () => {
+  _APP = new BasicWorld();
+});
