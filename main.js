1f289e3a32a46c1519c9af244941b9eab7eacd32
Zagoman <lucaszago.ph@gmail.com>
2022-03-12 17:02:46 +0100

    making cubes responsive

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function main() {
   const near = 0.1;
   const far = 5;
   const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-  camera.position.z = 4;
+  camera.position.z = 2;
   const scene = new THREE.Scene();
 
   const boxWidth = 1;
@@ -43,11 +43,26 @@ function main() {
     makeInstance(geometry, 0x8844aa, -2),
     makeInstance(geometry, 0xaa8844, 2),
   ];
+  function resizeRendererToDisplaySize(renderer) {
+    const canvas = renderer.domElement;
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    const needResize = canvas.width !== width || canvas.height !== height;
+    if (needResize) {
+      renderer.setSize(width, height, false);
+    }
+    return needResize;
+  }
   function render(time) {
     time *= 0.001; //convert time to seconds
 
     // cube.rotation.x = time;
     // cube.rotation.y = time;
+    if (resizeRendererToDisplaySize(renderer)) {
+      const canvas = renderer.domElement;
+      camera.aspect = canvas.clientWidth / canvas.clientHeight;
+      camera.updateProjectionMatrix();
+    }
 
     cubes.forEach((cube, ndx) => {
       const speed = 1 + ndx * 0.1;
